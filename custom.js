77a559f3cc313c1016627e8e546e3ac3953c7bf3
aporve <aporvep@gmail.com>
2021-10-12 17:39:15 +0530

    Testing 8

--- custom.js
+++ custom.js
@@ -33,7 +33,7 @@ function injectDynamicCssToParent() {
     var parentStyles = document.createElement('style');
     parentStyles.type = 'text/css';
 
-    var parentCssStyles = '#ymFrameHolder { width: 100%;} @media only screen and (min-width: 768px) { #ymFrameHolder { width: 440px; } #chatContainer {background: orange ; color:blue} }';
+    var parentCssStyles = '#ymFrameHolder { width: 100%;} @media only screen and (min-width: 768px) { #ymFrameHolder { width: 440px; }}';
     if (parentStyles.styleSheet) {
       parentStyles.styleSheet.cssText = parentCssStyles;
     } else {
@@ -203,7 +203,7 @@ window.addEventListener('message', function(eventData) {
                     var parentStyles = document.createElement('style');
                     parentStyles.type = 'text/css';
 
-                    var parentCssStyles = '#ymFrameHolder { width: 100%; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 440px; } #chatContainer {background: orange ; color:blue} }';
+                    var parentCssStyles = '#ymFrameHolder { width: 100%; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 440px; }  }';
                     if (parentStyles.styleSheet) {
                     parentStyles.styleSheet.cssText = parentCssStyles;
                     } else {
