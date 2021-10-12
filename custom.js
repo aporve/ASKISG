223606efa0e254253e0a2fa4a3dce04471b40e2a
aporve <aporvep@gmail.com>
2021-10-12 15:30:51 +0530

    Testing3

--- custom.js
+++ custom.js
@@ -182,7 +182,7 @@ window.addEventListener('message', function(eventData) {
                     var parentStyles = document.createElement('style');
                     parentStyles.type = 'text/css';
 
-                    var parentCssStyles = '#ymFrameHolder { width: 100%; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 880px; } , .card .card_title {background:orange} } }';
+                    var parentCssStyles = '#ymFrameHolder { width: 100%; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 880px; } , .card_title {background:orange} } }';
                     if (parentStyles.styleSheet) {
                     parentStyles.styleSheet.cssText = parentCssStyles;
                     } else {
@@ -203,7 +203,7 @@ window.addEventListener('message', function(eventData) {
                     var parentStyles = document.createElement('style');
                     parentStyles.type = 'text/css';
 
-                    var parentCssStyles = '#ymFrameHolder { width: 100%; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 440px; } , .card .card_title {background:red} }}';
+                    var parentCssStyles = '#ymFrameHolder { width: 100%; } @media only screen and (min-width: 768px) { #ymFrameHolder { width: 440px; } , .card_title {background:red} }}';
                     if (parentStyles.styleSheet) {
                     parentStyles.styleSheet.cssText = parentCssStyles;
                     } else {
